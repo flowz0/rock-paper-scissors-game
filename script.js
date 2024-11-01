1a0630aa5769eb883cacd78350367432338cd3cd
flowz0 <billylflowers@gmail.com>
2024-10-31 23:54:44 -0700

    Created a function for a randomComputerResult and a function to check if player has won the round

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function getRandomComputerResult() {
     const options = ["Rock", "Paper", "Scissors"];
 
+    return options[Math.floor(Math.random() * options.length)];
 }
 console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+    if (player === "Rock" && computer === "Scissors") {
+        return true;
+    } else if (player === "Scissors" && computer === "Paper") {
+        return true;
+    } else if (player === "Paper" && computer === "Rock") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
